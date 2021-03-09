cd7f7b0efc76e715331b390816a493080a0d675e
Giang Huynh <trgianghuynhdev1808@gmail.com>
2021-03-09 16:19:43 +0700

    imp: handle add qr, bar codes

--- index.js
+++ index.js
@@ -14,25 +14,62 @@ const setBackgroundImg = (url, canvas) => {
   });
 };
 
-const uploadFile = () => {
-  const inputEle = document.getElementById(INPUT_BACKGROUND_ID);
-  const file = inputEle.files[0];
-
+const uploadFile = (event) => {
+  const file = event.target.files[0];
   reader.readAsDataURL(file);
+};
+
+const formatImgObject = (img, canvas, scale) => {
+  const canvasCenter = canvas.getCenter();
+
+  img.scale(scale).set("flipX", true);
+  img.left = canvasCenter.left;
+  img.top = canvasCenter.top;
+  img.originX = "center";
+  img.originY = "center";
+};
+
+const generateQRCode = (url, canvas) => {
+  fabric.Image.fromURL(url, (img) => {
+    const QR_CODE_IMG_SCALE = 0.1;
+
+    formatImgObject(img, canvas, QR_CODE_IMG_SCALE);
+    canvas.add(img);
+    canvas.renderAll();
+  });
+};
+
+const generateBarCode = (url, canvas) => {
+  fabric.Image.fromURL(url, (img) => {
+    const BAR_CODE_IMG_SCALE = 0.5;
+
+    formatImgObject(img, canvas, BAR_CODE_IMG_SCALE);
+    canvas.add(img);
+    canvas.renderAll();
+  });
 };
 
 const CANVAS_ID = "canvas";
 const INPUT_BACKGROUND_ID = "background-upload";
+const QR_CODE_BTN_ID = "qr-code-btn";
+const BAR_CODE_BTN_ID = "bar-code-btn";
+
+const QR_CODE_URL = "./public/imgs/qr-code.png";
+const BAR_CODE_URL = "./public/imgs/bar-code.png";
 
 const inputFile = document.getElementById(INPUT_BACKGROUND_ID);
+const qrCodeButton = document.getElementById(QR_CODE_BTN_ID);
+const barCodeButton = document.getElementById(BAR_CODE_BTN_ID);
+
 const reader = new FileReader();
 const canvas = initCanvas(CANVAS_ID);
 
-//Event Listener
+//Event Listeners
 inputFile.addEventListener("change", uploadFile);
-reader.addEventListener("load", () => {
-  const curImg = new Image();
 
+reader.addEventListener("load", () => {
+  //create Image to get height, width
+  const curImg = new Image();
   curImg.src = reader.result;
 
   curImg.onload = function () {
@@ -43,3 +80,11 @@ reader.addEventListener("load", () => {
     setBackgroundImg(reader.result, canvas);
   };
 });
+
+qrCodeButton.addEventListener("click", () =>
+  generateQRCode(QR_CODE_URL, canvas)
+);
+
+barCodeButton.addEventListener("click", () =>
+  generateBarCode(BAR_CODE_URL, canvas)
+);
